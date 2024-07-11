e3af874317e5ffaafc346abb20dc3d046010e96e
Maulana Muhammad <maulllanamuhammad@gmail.com>
2024-07-11 10:24:11 +0700

    fix : response CreateTransactionAPI

--- DebtTrack.Application/Features/TransactionFeatures/Command/CreateTransaction/CreateTransactionResponse.cs
+++ DebtTrack.Application/Features/TransactionFeatures/Command/CreateTransaction/CreateTransactionResponse.cs
@@ -2,6 +2,8 @@ namespace DebtTrack.Application.Features.DebtFeatures.Command.CreateDebt;
 
 public sealed record CreateTransactionResponse
 {
+    public int ActivityId { get; init; }
+    public int ParticipantId { get; init; }
     public string Amount { get; init; }
     public string Description { get; init; }
     public bool IsPaid { get; init; }
